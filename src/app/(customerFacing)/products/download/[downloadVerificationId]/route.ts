423034499e055d78112b3315b4f15b72991b835b
Michael Laufersweiler <michael.laufersweiler@lulafit.com>
2024-07-22 15:10:03 -0500

    adding process.cwd() to download program

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,30 +1,35 @@
-import db from "@/db/db"
-import { NextRequest, NextResponse } from "next/server"
-import fs from "fs/promises"
+import db from "@/db/db";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises";
+import path from "path"; // Import the path module
 
 export async function GET(
     req: NextRequest,
-    {
-        params: { downloadVerificationId },
-    }: { params: { downloadVerificationId: string } }
+    { params: { downloadVerificationId } }: { params: { downloadVerificationId: string } }
 ) {
-    const data = await db.downloadVerification.findUnique({
-        where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
-        select: { product: { select: { filePath: true, name: true } } },
-    })
+    try {
+        const data = await db.downloadVerification.findUnique({
+            where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+            select: { product: { select: { filePath: true, name: true } } },
+        });
 
-    if (data == null) {
-        return NextResponse.redirect(new URL("/products/download/expired", req.url))
+        if (data == null) {
+            return NextResponse.redirect(new URL("/products/download/expired", req.url));
+        }
+
+        const pathToFile = path.join(process.cwd(), data.product.filePath);
+        const { size } = await fs.stat(pathToFile);
+        const file = await fs.readFile(pathToFile);
+        const extension = data.product.filePath.split(".").pop();
+
+        return new NextResponse(file, {
+            headers: {
+                "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+                "Content-Length": size.toString(),
+            },
+        });
+    } catch (error) {
+        console.error("Error handling download request:", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
     }
-
-    const { size } = await fs.stat(data.product.filePath)
-    const file = await fs.readFile(data.product.filePath)
-    const extension = data.product.filePath.split(".").pop()
-
-    return new NextResponse(file, {
-        headers: {
-            "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-            "Content-Length": size.toString(),
-        },
-    })
 }
