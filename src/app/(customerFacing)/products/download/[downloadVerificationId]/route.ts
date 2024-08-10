b760f3610e6eb2c8371d0f2c95ebbb7bccb843ba
Michael Laufersweiler <michael.laufersweiler@lulafit.com>
2024-08-10 15:44:03 -0400

    logging out /download route.ts try catch

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -4,8 +4,6 @@ import db from "@/db/db"
 
 export async function GET(req: NextRequest, { params: { downloadVerificationId }, }: { params: { downloadVerificationId: string } }) {
 
-    console.log(`downloadVerificationId: ${downloadVerificationId}`);
-
     const data = await db.downloadVerification.findUnique({
         where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
         select: { product: { select: { filePath: true, name: true } } },
@@ -17,21 +15,40 @@ export async function GET(req: NextRequest, { params: { downloadVerificationId }
 
     // Sanitize the filePath by replacing spaces with underscores or removing them
     let sanitizedFilePath = data.product.filePath.replace(/\s+/g, '_'); // or use '' to remove spaces entirely
-    console.log(`Resolved and sanitized path to file: ${sanitizedFilePath}`);
+
+    console.log(`Original file path: ${data.product.filePath}`);
+    console.log(`Sanitized file path: ${sanitizedFilePath}`);
 
     try {
-        const { size } = await fs.stat(sanitizedFilePath)
-        const file = await fs.readFile(sanitizedFilePath)
-        const extension = sanitizedFilePath.split(".").pop()
+        // Attempt to read the file at the original path
+        const { size } = await fs.stat(data.product.filePath);
+        const file = await fs.readFile(data.product.filePath);
+        const extension = data.product.filePath.split(".").pop();
 
         return new NextResponse(file, {
             headers: {
                 "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
                 "Content-Length": size.toString(),
             },
-        })
+        });
     } catch (error) {
-        console.error(`Error reading file at path: ${sanitizedFilePath}`, error);
+        console.error(`Error reading file at original path: ${data.product.filePath}`, error);
+    }
+
+    try {
+        // Attempt to read the file at the sanitized path
+        const { size } = await fs.stat(sanitizedFilePath);
+        const file = await fs.readFile(sanitizedFilePath);
+        const extension = sanitizedFilePath.split(".").pop();
+
+        return new NextResponse(file, {
+            headers: {
+                "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+                "Content-Length": size.toString(),
+            },
+        });
+    } catch (error) {
+        console.error(`Error reading file at sanitized path: ${sanitizedFilePath}`, error);
         return NextResponse.redirect(new URL("/products/download/error", req.url));
     }
 }
