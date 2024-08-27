18f50ee3ba4a843975c0a4d4fa71822eb634f159
Michael Laufersweiler <michael.laufersweiler@lulafit.com>
2024-08-26 20:34:40 -0500

    updating add products action

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -20,25 +20,24 @@ const addSchema = z.object({
 })
 
 export async function addProduct(prevState: unknown, formData: FormData) {
-    const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+    const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
     if (result.success === false) {
-        return result.error.formErrors.fieldErrors;
+        return result.error.formErrors.fieldErrors
     }
 
-    const data = result.data;
+    const data = result.data
 
-    // Ensure products directory in public exists
-    await fs.mkdir("public/products", { recursive: true });
+    await fs.mkdir("products", { recursive: true })
+    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
 
-    // Store the file in the public/products directory
-    const filePath = `/products/${crypto.randomUUID()}-${data.file.name}`;
-    await fs.writeFile(`public${filePath}`, Buffer.from(await data.file.arrayBuffer()));
+    await fs.mkdir("public/products", { recursive: true })
+    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+    await fs.writeFile(
+        `public${imagePath}`,
+        Buffer.from(await data.image.arrayBuffer())
+    )
 
-    // Store the image in the public/products directory
-    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
-    await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()));
-
-    // Save to database with paths that are relative to the public directory
     await db.product.create({
         data: {
             isAvailableForPurchase: false,
@@ -48,15 +47,13 @@ export async function addProduct(prevState: unknown, formData: FormData) {
             filePath,
             imagePath,
         },
-    });
+    })
 
-    revalidatePath("/");
-    revalidatePath("/products");
+    revalidatePath("/")
+    revalidatePath("/products")
 
-    redirect("/admin/products");
+    redirect("/admin/products")
 }
-
-
 
 const editSchema = addSchema.extend({
     file: fileSchema.optional(),
@@ -68,34 +65,31 @@ export async function updateProduct(
     prevState: unknown,
     formData: FormData
 ) {
-    const result = editSchema.safeParse(Object.fromEntries(formData.entries()));
+    const result = editSchema.safeParse(Object.fromEntries(formData.entries()))
     if (result.success === false) {
-        return result.error.formErrors.fieldErrors;
+        return result.error.formErrors.fieldErrors
     }
 
-    const data = result.data;
-    const product = await db.product.findUnique({ where: { id } });
+    const data = result.data
+    const product = await db.product.findUnique({ where: { id } })
 
-    if (product == null) return notFound();
+    if (product == null) return notFound()
 
-    let filePath = product.filePath;
+    let filePath = product.filePath
     if (data.file != null && data.file.size > 0) {
-        // Delete the old file
-        await fs.unlink(`public${product.filePath}`);
-
-        // Store the new file in public/products directory
-        filePath = `/products/${crypto.randomUUID()}-${data.file.name}`;
-        await fs.writeFile(`public${filePath}`, Buffer.from(await data.file.arrayBuffer()));
+        await fs.unlink(product.filePath)
+        filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+        await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
     }
 
-    let imagePath = product.imagePath;
+    let imagePath = product.imagePath
     if (data.image != null && data.image.size > 0) {
-        // Delete the old image
-        await fs.unlink(`public${product.imagePath}`);
-
-        // Store the new image in public/products directory
-        imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
-        await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()));
+        await fs.unlink(`public${product.imagePath}`)
+        imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+        await fs.writeFile(
+            `public${imagePath}`,
+            Buffer.from(await data.image.arrayBuffer())
+        )
     }
 
     await db.product.update({
@@ -107,14 +101,13 @@ export async function updateProduct(
             filePath,
             imagePath,
         },
-    });
+    })
 
-    revalidatePath("/");
-    revalidatePath("/products");
+    revalidatePath("/")
+    revalidatePath("/products")
 
-    redirect("/admin/products");
+    redirect("/admin/products")
 }
-
 
 export async function toggleProductAvailability(
     id: string,
@@ -127,14 +120,13 @@ export async function toggleProductAvailability(
 }
 
 export async function deleteProduct(id: string) {
-    const product = await db.product.delete({ where: { id } });
+    const product = await db.product.delete({ where: { id } })
 
-    if (product == null) return notFound();
+    if (product == null) return notFound()
 
-    // Delete the file and image from public/products directory
-    await fs.unlink(`public${product.filePath}`);
-    await fs.unlink(`public${product.imagePath}`);
+    await fs.unlink(product.filePath)
+    await fs.unlink(`public${product.imagePath}`)
 
-    revalidatePath("/");
-    revalidatePath("/products");
+    revalidatePath("/")
+    revalidatePath("/products")
 }
